cd666744f23cf55f6638d38298251337f776c63e
Pistolmannen <erikgustafsson03@gmail.com>
2024-05-07 16:13:23 +0200

    picture can be shown

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView URLView = (ImageView) findViewById(R.id.URLPicture);
+
+        Glide.with(this).load("https://i.pinimg.com/736x/e5/5e/0a/e55e0a30fe66a81b6d7adc1cd9df252e.jpg").into(URLView);
+
     }
 }
